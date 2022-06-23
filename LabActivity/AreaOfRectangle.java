e82e3fad5b1e488e51dce68273e6268ef52f2642
farzanashaikh05 <107574509+farzanashaikh05@users.noreply.github.com>
2022-06-23 14:03:50 +0530

    Add files via upload

---
+++ LabActivity/AreaOfRectangle.java
@@ -0,0 +1,24 @@
+package javacourse;
+/*
+ * @Author Farzana Shaikh
+ */
+public class AreaOfRectangle {
+
+	public static void main(String[] args) {
+		area();//calling method
+	
+		
+
+	}
+
+	//defining the method area()
+	private static void area() {
+		
+		int length=6,breadth=4;//declare the length and breadth
+		
+		int Area=length*breadth;//formula for finding the area of rectangle
+		
+		System.out.println(Area);//printing the Area 
+	}
+
+}
