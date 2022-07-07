9e992167efdd71809c4563b7a4bde484c5baf2a1
farzanashaikh05 <107574509+farzanashaikh05@users.noreply.github.com>
2022-07-07 14:24:20 +0530

    Add files via upload

---
+++ Assignment/OddNumber.java
@@ -0,0 +1,16 @@
+package com.cognizant.tax;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		int i;
+		for (i = 0;i<=200 ; i++) {
+			if(i%2!=0) {
+				System.out.println(i);
+			}
+			
+		}
+
+	}
+
+}
