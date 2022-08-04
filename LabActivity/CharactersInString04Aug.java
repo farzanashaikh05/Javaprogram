2ad029732f9a8d21eb1d963b241466511d86ea9b
Farzana Shaikh <107574509+farzanashaikh05@users.noreply.github.com>
2022-08-04 14:08:25 +0530

    Add files via upload

---
+++ LabActivity/CharactersInString04Aug.java
@@ -0,0 +1,23 @@
+package Farzana;
+/*
+ * @Author Farzana Shaikh
+ */
+
+//2. Java Program to count the total number of characters in a string
+public class CharactersInString04Aug {
+
+	public static void main(String[] args) {
+		
+		countchar();
+	
+	}
+
+	private static void countchar() {
+		String str="Shaikh Farzana ";
+
+		System.out.println(str);
+		str=str.replace(" ", "");
+		System.out.println("Characters in a String is :"+str.length());
+	}
+
+}
